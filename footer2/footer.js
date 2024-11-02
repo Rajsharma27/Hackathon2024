2a7bbb6c55971784cb789c1f91b26fb7f28ffa8a
Rajsharma27 <sharmaraj.sm@gmail.com>
2024-11-02 22:03:11 +0530

    Update footer.js

--- footer2/footer.js
+++ footer2/footer.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     links.forEach(link => {
         link.addEventListener('click', function (event) {
-            // Opens external links in a new tab
+           
             const href = link.getAttribute('href');
             if (href && href.startsWith('http')) {
                 event.preventDefault();
